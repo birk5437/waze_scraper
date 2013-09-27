8c6ffaa91d7667908f1a327c8b78a2b191336b15
Burke Ramsey <birk5437@gmail.com>
2013-09-27 14:16:10 -0400

    FINALLY parsing right

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,12 @@
 require 'rubygems'
+gem 'libxml-ruby', '>= 0.8.3'
 require 'json'
 require 'rss'
 require 'open-uri'
 require 'active_support/all'
 require 'net/http'
 require 'nokogiri'
+require 'xml'
 
 url = "http://www.waze.com/rtserver/web/GeoRSS?format=JSON&mj=10&ma=10&jmds=120&jmu=0&left=-9902228.4683365&right=-9233863.0931305&bottom=4707736.5908183&top=5045282.5076653&sc=1733376&pr=Mercator"
 
@@ -14,19 +16,47 @@ url = "http://www.waze.com/rtserver/web/GeoRSS?format=JSON&mj=10&ma=10&jmds=120&
 #response = Net::HTTP.new(uri.host, uri.port).start{ |http| http.request(request) }
 #puts response.body
 f = File.open("response.txt", 'r')
-doc = Nokogiri::XML(f)
+#doc = Nokogiri::XML(f)
 #f.write(response.body)
 #f.close
 
-puts doc.at('linqmap:type').detect{ |i| i.text == "POLICEMAN" }
+parser = XML::Parser.string(f.read, :options =>XML::Parser::Options::RECOVER)
 
-feed = RSS::Parser.parse(f, false)
-puts "TITLE: #{feed.channel.title}"
-puts "ITEM:"
-puts feed.items.first
-cop = feed.items.detect{ |i| i.send(:"linqmap:type") == "POLICEMAN" }
-puts "COP:"
-puts cop
+#puts doc.at('linqmap:type').detect{ |i| i.text == "POLICEMAN" }
+doc = parser.parse
+doc.find("channel").first.find("item").each do |item|
+  puts item.find("linqmap:type").first
+  #and just guessing you want that url thingy
+  # puts item.find("media:content").first.attributes.get_attribute("url").value
+end
+
+# feed = RSS::Parser.parse(f, false)
+# puts "TITLE: #{feed.channel.title}"
+# puts "ITEM:"
+# puts feed.items.first
+# feed.items.each do |i|
+#   puts "1"
+#   begin
+#     puts i.linqmap[:type]
+#   rescue
+#     nil
+#   end
+#   puts "2"
+#   begin
+#     puts i.linqmap['type']
+#   rescue
+#     nil
+#   end
+#   puts "3"
+#   begin
+#     puts i.linqmap_type
+#   rescue
+#     nil
+#   end
+# end
+#cop = feed.items.detect{ |i| i.linqmap[:type] == "POLICEMAN" }
+#puts "COP:"
+#puts cop
 
 #open(url) do |rss|
 #  feed = RSS::Parser.parse(rss)
